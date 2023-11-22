e16a05e18ed34d8e17ba6bc9fdbc7c795d0ab239
Sergio22U <113546908+Sergio22U@users.noreply.github.com>
2023-11-22 20:19:03 +0100

    estilos

--- Program/View/Index.php
+++ Program/View/Index.php
@@ -12,103 +12,116 @@ if (!isset($_SESSION['carrito'])) {
 }
 
 
-echo "<form method=\"get\">";
-echo "<label for=\"nombre\">Buscar por nombre:</label>";
-echo "<input type=\"text\" name=\"nombre\" id=\"nombre\">";
-echo "<br>";
-echo "<label for=\"precio_min\">Precio mínimo:</label>";
-echo "<input type=\"number\" name=\"precio_min\" id=\"precio_min\">";
-echo "<br>";
-echo "<label for=\"precio_max\">Precio máximo:</label>";
-echo "<input type=\"number\" name=\"precio_max\" id=\"precio_max\">";
-echo "<br>";
-echo "<input type=\"submit\" value=\"Buscar\">";
-echo "</form>";
-echo "<form action=\"logout.php\"><input type=\"submit\" value=\"Cerrar sesión\"></form><br>";
 
 $listado = arrayClass(
     isset($_GET['nombre']) ? $_GET['nombre'] : null,
     isset($_GET['precio_max']) ? $_GET['precio_max'] : null,
     isset($_GET['precio_min']) ? $_GET['precio_min'] : null
 );
-
-foreach ($listado as $item) {
-    echo "Nombre del producto: " . $item->getName() . "<br>";
-    echo "Descripción del producto: " . $item->getDescription() . "<br>";
-    echo "Precio del producto: " . $item->getPrice() . "<br><br><br>";
-}
 ?>
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Mi Tienda en Línea</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+            .sticky-sidebar {
+                position: -webkit-sticky;
+            position: sticky;
+            top: 0;
+            z-index: 1000; /* Ajusta según tus necesidades */
+            transition: top 0.3s; /* Agregamos una transición para suavizar el movimiento */
+        }
+    </style>
 </head>
 <body>
-
-    <!-- Barra de navegación mejorada -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Mi Tienda</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Inicio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Productos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Carrito</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contacto</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-
-    <!-- Banner de bienvenida -->
-    <div class="jumbotron jumbotron-fluid text-center bg-info text-white">
-        <div class="container">
-            <h1 class="display-4">Bienvenido a Mi Tienda en Línea</h1>
-            <p class="lead">Explora nuestra increíble selección de productos.</p>
-        </div>
+<!-- Menú de navegación -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Mi Tienda en Línea</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Inicio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="../View/formPago.php"><i class="fa fa-shopping-cart" style="font-size:24px"></i></a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="../View/logout.php">
+            <i class="fa fa-sign-out" style="font-size:20px;color:red"></i>
+                </a>
+            </li>
+        </ul>
     </div>
+</nav>
 
-    <!-- Sección de productos -->
-    <div class="container">
-        <div class="row">
-            <!-- Producto 1 -->
-            <?php foreach ($listado as $item) { ?>
-    <div class='col-md-4'>
-        <div class='card mb-4 shadow-sm'>
-            <img src='https://via.placeholder.com/300' class='card-img-top' alt='...'>
-            <div class='card-body'>
-                <h5 class='card-title'><?php echo $item->getName(); ?></h5>
-                <p class='card-text'><?php echo $item->getDescription(); ?></p>
-                <p class='card-text'><?php echo $item->getPrice(); ?> €</p>
-                <div class='d-flex justify-content-between align-items-center'>
-                    <div class='btn-group'>
-                    <a class='btn btn-sm btn-outline-primary' href="../index.php?agregarAlCarrito=<?php echo urlencode(json_encode($item)); ?>">Agregar al carrito</a>
-                    </div>
+<!-- Sección de productos y menú lateral de búsqueda -->
+<div class="container mt-3">
+    <div class="row">
+        <!-- Menú lateral de búsqueda -->
+        <div class="col-md-3 sticky-sidebar top" >
+            <div class="card mb-4">
+                <div class="card-body">
+                    <h5 class="card-title">Filtrar Productos</h5>
+                    <form method="get">
+                        <label for="nombre">Buscar por nombre:</label>
+                        <input type="text" name="nombre" id="nombre" class="form-control">
+                        <br>
+                        <label for="precio_min">Precio mínimo:</label>
+                        <input type="number" name="precio_min" id="precio_min" class="form-control">
+                        <br>
+                        <label for="precio_max">Precio máximo:</label>
+                        <input type="number" name="precio_max" id="precio_max" class="form-control">
+                        <br>
+                        <input type="submit" value="Buscar" class="btn btn-primary">
+                    </form>
                 </div>
             </div>
         </div>
-    </div>
-<?php } 
-?>
-    </div>
 
-    <!-- Agrega la referencia a Bootstrap JS y Popper.js -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+        <!-- Productos -->
+        <div class="col-md-9">
+            <div class="row">
+                <?php foreach ($listado as $item) { ?>
+                    <div class='col-md-4'>
+                        <div class='card mb-4 shadow-sm'>
+                            <img src='https://via.placeholder.com/300' class='card-img-top' alt='...'>
+                            <div class='card-body'>
+                                <h5 class='card-title'><?php echo $item->getName(); ?></h5>
+                                <p class='card-text'><?php echo $item->getDescription(); ?></p>
+                                <p class='card-text'><?php echo $item->getPrice(); ?> €</p>
+                                <div class='d-flex justify-content-between align-items-center'>
+                                    <div class='btn-group'>
+                                        <a class='btn btn-sm btn-outline-primary' href="../index.php?agregarAlCarrito=<?php echo urlencode(json_encode($item)); ?>">Agregar al carrito</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+        window.onscroll = function() {
+            var sidebar = document.getElementById("sticky-sidebar");
+            if (window.pageYOffset > 20) {
+                sidebar.style.top = "20px";
+            } else {
+                sidebar.style.top = "0";
+            }
+        };
+    </script>
+<!-- Agrega la referencia a Bootstrap JS y Popper.js -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </body>
 </html>
-
