f62a61b4065f5b915918a3fa04901205cd51d4d6
Sergio22U <113546908+Sergio22U@users.noreply.github.com>
2023-11-22 18:02:05 +0100

    estilo login

--- Program/View/login.php
+++ Program/View/login.php
@@ -1,11 +1,8 @@
 <?php
-// archivo: login.php
 session_start();
 require_once '../Business/UsuarioServicio.php';
 
 if (isset($_POST['submit'])) {
-    
-    
     $data = iniciar_sesion($_POST['correo'], $_POST['contrasena']);
     if ($data) {
         $_SESSION['usu_id'] = $data->getId();
@@ -18,19 +15,49 @@ if (isset($_POST['submit'])) {
     }
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
-<body>
-    <h2>Login</h2>
-    <form method="post" action="login.php">
-        Correo: <input type="text" name="correo" required><br>
-        Contraseña: <input type="password" name="contrasena" required><br>
-        <input type="submit" name="submit" value="Iniciar sesión">
-    </form>
-    <?php if (isset($error)) echo $error; ?>
+<body class="bg-light">
+
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">
+                    <h2 class="text-center">Login</h2>
+                </div>
+                <div class="card-body">
+                    <form method="post" action="login.php">
+                        <div class="form-group">
+                            <label for="correo">Correo:</label>
+                            <input type="text" class="form-control" name="correo" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="contrasena">Contraseña:</label>
+                            <input type="password" class="form-control" name="contrasena" required>
+                        </div>
+                        <div class="text-center">
+                            <button type="submit" name="submit" class="btn btn-primary">Iniciar sesión</button>
+                        </div>
+                    </form>
+                    <?php if (isset($error)) echo "<div class='alert alert-danger mt-3'>$error</div>"; ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Agrega la referencia a Bootstrap JS y Popper.js -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
 </body>
 </html>
