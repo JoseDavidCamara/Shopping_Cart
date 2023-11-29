4c10c4e447a56b0cd05d75a59013e37b82160c90
JoseDavidCamara <trastin.2003@gmail.com>
2023-11-29 18:24:30 +0100

    log in arreglado

--- Program/Business/UsuarioServicio.php
+++ Program/Business/UsuarioServicio.php
@@ -1,33 +1,41 @@
 <?php
 require_once '../DataAccess/UsuarioDAO.php';
 
-class UsuarioServicio {
+class UsuarioServicio
+{
     private $id;
     private $nombre;
     private $correo;
 
-    public function __construct($id,$nombre,$correo) {
+    public function __construct($id, $nombre, $correo)
+    {
         $this->id = $id;
         $this->nombre = $nombre;
         $this->correo = $correo;
     }
-    public function getId() {
+    public function getId()
+    {
         return $this->id;
     }
-    public function getNombre() {
+    public function getNombre()
+    {
         return $this->nombre;
     }
-    public function getCorreo() {
+    public function getCorreo()
+    {
         return $this->correo;
     }
-    
-} 
-function CrearUsuario($nombre, $correo, $contrasena) {
+}
+function CrearUsuario($nombre, $correo, $contrasena)
+{
     $usuario_dao = new UsuarioDAO();
     $usuario_dao->insertar_usuario($nombre, $correo, $contrasena);
 }
-function iniciar_sesion($correo, $contrasena) {
+function iniciar_sesion($correo, $contrasena)
+{
     $usuario_dao = new UsuarioDAO();
-     $data = $usuario_dao->validar_credenciales($correo, $contrasena);
-    return new UsuarioServicio($data['id_usuario'], $data['nombre'], $data['email']);
+    $data = $usuario_dao->validar_credenciales($correo, $contrasena);
+    if ($data) {
+        return new UsuarioServicio($data['id_usuario'], $data['nombre'], $data['email']);
+    }
 }
