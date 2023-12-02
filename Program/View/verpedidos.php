16cd7a35cb8eba6f674d088482c4010eb26cbd54
Sergio22U <113546908+Sergio22U@users.noreply.github.com>
2023-12-02 09:03:01 +0100

    verpedidos

--- Program/View/verpedidos.php
+++ Program/View/verpedidos.php
@@ -93,59 +93,71 @@ $modoOscuroCookie = isset($_COOKIE['modo_oscuro']) ? $_COOKIE['modo_oscuro'] : '
             color: #ccc;
             /* Color de texto gris claro en modo oscuro */
         }
+
+        .dark-mode {
+    background-color: #333; /* Color de fondo oscuro */
+    color: #fff; /* Texto blanco */
+    /* Agrega otros estilos específicos de modo oscuro según tus necesidades */
+}
     </style>
 </head>
 
 <body class="<?php echo $modoOscuroCookie === 'true' ? 'dark-mode' : ''; ?>">
-    <div class="container">
-        <form class="form-inline float-left" method="get" action="">
-            <div class="form-group mx-sm-3 mb-2">
-                <label for="fecha" class="sr-only">Filtrar por fecha:</label>
-                <input type="date" class="form-control" id="fecha" name="fecha" value="<?php echo isset($_GET['fecha']) ? htmlspecialchars($_GET['fecha']) : ''; ?>">
+<div class="container">
+<div class="row">
+        <div class="col-md-3">
+            <div class="card mt-3  <?php echo ($modoOscuroCookie === 'true') ? 'dark-mode' : ''; ?>">
+                <div class="card-body p-2"> <!-- Reducir el padding del cuerpo de la tarjeta -->
+                    <h5 class="card-title <?php echo ($modoOscuroCookie === 'true') ? 'text-white' : ''; ?>">Filtrar por fecha</h5>
+                    <form class="form-inline" method="get" action="">
+                        <div class="form-group mx-1 mb-2"> <!-- Reducir los márgenes horizontal y vertical del grupo de formulario -->
+                            <label for="fecha" class="sr-only">Fecha:</label>
+                            <input type="date" class="form-control">
+                        </div>
+                        <button type="submit" class="btn btn-primary mb-2">Filtrar</button>
+                    </form>
+                </div>
             </div>
-            <button type="submit" class="btn btn-primary mb-2">Filtrar</button>
-        </form>
-    </div>
+        </div>
+        <div class="col-md-9">
+            <?php
+            // Tu código PHP para obtener la lista de pedidos
+            $fechaFiltro = isset($_GET['fecha']) ? $_GET['fecha'] : null;
+            $listado = listaPedido($_SESSION['usu_id'], $fechaFiltro);
 
-    <div class="container">
+            echo '<h1>Historial de Pedidos</h1>';
 
-        <?php
-        // Tu código PHP para obtener la lista de pedidos
-        $fechaFiltro = isset($_GET['fecha']) ? $_GET['fecha'] : null;
-        $listado = listaPedido($_SESSION['usu_id'], $fechaFiltro);
+            // Creamos un array para agrupar los pedidos por fecha
+            $pedidosPorFecha = [];
 
-        echo '<h1>Historial de Pedidos</h1>';
+            foreach ($listado as $pedido) {
+                $fecha = $pedido->getFechaPedido();
 
-        // Creamos un array para agrupar los pedidos por fecha
-        $pedidosPorFecha = [];
+                if (!isset($pedidosPorFecha[$fecha])) {
+                    $pedidosPorFecha[$fecha] = [];
+                }
 
-        foreach ($listado as $pedido) {
-            $fecha = $pedido->getFechaPedido();
-
-            if (!isset($pedidosPorFecha[$fecha])) {
-                $pedidosPorFecha[$fecha] = [];
+                $pedidosPorFecha[$fecha][] = $pedido;
             }
 
-            $pedidosPorFecha[$fecha][] = $pedido;
-        }
+            // Mostramos los pedidos agrupados por fecha
+            foreach ($pedidosPorFecha as $fecha => $pedidos) {
+                echo '<div class="pedido-container ' . ($modoOscuroCookie === 'true' ? 'dark-mode' : '') . '">';
+                echo '<h3>Pedido realizado el: ' . $fecha . '</h3>';
 
-        // Mostramos los pedidos agrupados por fecha
-        foreach ($pedidosPorFecha as $fecha => $pedidos) {
-            echo '<div class="pedido-container ' . ($modoOscuroCookie === 'true' ? 'dark-mode' : '') . '">';
-            echo '<h3>Pedido realizado el: ' . $fecha . '</h3>';
+                foreach ($pedidos as $pedido) {
+                    echo '<div class="pedido-item ' . ($modoOscuroCookie === 'true' ? 'dark-mode' : '') . '">';
+                    echo '<h4>' . $pedido->getNombreProducto() . '</h4>';
+                    echo '<p>Cantidad: ' . $pedido->getCantidad() . '</p>';
+                    echo '</div>';
+                }
 
-            foreach ($pedidos as $pedido) {
-                echo '<div class="pedido-item ' . ($modoOscuroCookie === 'true' ? 'dark-mode' : '') . '">';
-                echo '<h4>' . $pedido->getNombreProducto() . '</h4>';
-                echo '<p>Cantidad: ' . $pedido->getCantidad() . '</p>';
                 echo '</div>';
             }
-
-            echo '</div>';
-        }
-        ?>
+            ?>
+        </div>
     </div>
-
+</div>
 </body>
 
 </html>
